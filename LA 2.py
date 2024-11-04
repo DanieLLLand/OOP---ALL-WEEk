1dac2934214ded9371a8102bbb1f7c1f0caa4744
daniel <lomibaodaniel4@gmail.com>
2024-11-04 14:55:45 +0800

    Add files via upload

---
+++ LA 2.py
@@ -0,0 +1,10 @@
+print("LA 2")
+
+class Hero:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+hero = Hero("Nana", "Mage")
+print("Name:", hero.name)
+print("Role:", hero.role)
