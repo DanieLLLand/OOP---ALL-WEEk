680da0e1bb0d69ba6e41e4d0114f12335e0315ec
daniel <lomibaodaniel4@gmail.com>
2024-11-04 14:56:18 +0800

    Add files via upload

---
+++ LA 4.py
@@ -0,0 +1,9 @@
+print("LA 4")
+
+class character:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def __str__(self):
+        return "{hero.name} is a {self.role} hero. "
